1cd17a0b7e3bfceea83f85edd6e8bf000d1f05f0
Liam Ha <haduylinh2005.work@gmail.com>
2024-01-15 18:30:00 -0500

    add get teams from API

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,35 +1,31 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function Card ({teamData}){
     return (
 
             <div className='hero--card'>
-                <h3>Team ID: {teamData.id}</h3>
-                <h3>Team name: {teamData.name}</h3>
                 <img src={teamData.img} alt={`Team logo of ${teamData.name}`} />
+                <h3>{teamData.name}</h3>
             </div>
 
     )
 }
 
 function Hero (){
-    const [stats, setStats] = useState([{
-        'country': 'United States',
-        'id': '5248',
-        'img': 'https://owcdn.net/img/62a409ad29351.png',
-        'name': 'Evil Geniuses',
-        'url': 'https://www.vlr.gg/team/5248/evil-geniuses',
-    },
-    {
-        'country': 'United States',
-        'id': '5249',
-        'img': 'https://owcdn.net/img/62a409ad29351.png',
-        'name': 'Evil Geniuses',
-        'url': 'https://www.vlr.gg/team/5248/evil-geniuses',
-    }
-]);
+    const [stats, setStats] = useState([]);
+
+    useEffect(()=> {
+        fetch("https://vlr.orlandomm.net/api/v1/teams?" + new URLSearchParams({
+            limit: 20,
+            region: 'na',
+        }))
+        .then(res => res.json())
+        .then(data => setStats(data['data']))
+
+    }, [stats])
+
     return (
 
         <div className='hero--container'>
